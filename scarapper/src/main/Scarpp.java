dbf4f5aebbf7220641f629d2359925cca036992a
deva <lali.pera@gmail.com>
2017-02-25 12:33:58 +0530

    init2

---
+++ scarapper/src/main/Scarpp.java
@@ -0,0 +1,32 @@
+package scarapper;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Scarpp {
+	public static void main(String[] args) throws IOException {
+		Document doc = null;
+
+		String baseUrl = "http://www.300mbfilms.co/page/";
+		
+		for(int i = 1; i <100 ; i++){
+			doc = Jsoup.connect(baseUrl+i).get();
+			Elements contents = doc.select("div#content");
+
+			for (Element content : contents) {
+				Elements titles = content.select("h2.title");
+				for (Element title : titles) {
+
+					System.out.println(title.select("a[title]").text());
+
+				}
+
+			}
+		}
+
+	}
+}
